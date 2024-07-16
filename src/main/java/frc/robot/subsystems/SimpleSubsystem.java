9e47f9f2aeaa877d111acfca2d1966ff9bced3fc
spybh66 <brysonh99@gmail.com>
2024-07-16 17:29:42 -0400

    Simple Subsystem frame work init

---
+++ src/main/java/frc/robot/subsystems/SimpleSubsystem.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+public class SimpleSubsystem extends SubsystemBase {
+
+  @RequiredArgsConstructor
+  public enum State {
+    ON(() -> 1.0),
+    OFF(() -> 0.0);
+
+    private final DoubleSupplier outputSupplier;
+
+    private double getStateOutput() {
+      return outputSupplier.getAsDouble();
+    }
+  }
+  @Getter @Setter
+  private State state = State.OFF;
+
+  /** Creates a new SimpleSubsystem. */
+  public SimpleSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
