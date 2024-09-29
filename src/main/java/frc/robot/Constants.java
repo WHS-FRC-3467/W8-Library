4be0efee0d6c2bfcca7b60320bfa633f049dfe53
Bryson Halsey <brysonh99@gmail.com>
2024-09-28 20:39:33 -0400

    Fixed constants merge

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,9 +15,20 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import frc.robot.generated.TunerConstants;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import frc.robot.generated.TunerConstants;
+//Test
+
+/** Add your docs here. */
 public class Constants {
-    
-    public static final Mode currentMode = Mode.REAL;
+
+        public static final Mode currentMode = Mode.REAL;
 
     public static enum Mode {
         REAL,
@@ -25,25 +36,7 @@ public class Constants {
         REPLAY
     }
 
-    public static final class DriveConstants {
-        public static final double headingAngleTolerance = 2.0;
-        public static final double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
-        public static final double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
-        public static TalonFXConfiguration motorConfig() {
-            TalonFXConfiguration m_configuration = new TalonFXConfiguration();
-
-            m_configuration.CurrentLimits.SupplyCurrentLimit = 30;
-            m_configuration.CurrentLimits.SupplyCurrentThreshold = 90;
-            m_configuration.CurrentLimits.SupplyTimeThreshold = 0.01;
-            m_configuration.CurrentLimits.SupplyCurrentLimitEnable = true;
-            m_configuration.CurrentLimits.StatorCurrentLimit = 80;
-            m_configuration.CurrentLimits.StatorCurrentLimitEnable = true;
-
-            return m_configuration;
-        }
-    }
- 
     public static final class ExampleSimpleSubsystemConstants {
         public static final int ID_Motor = 0;
 
@@ -94,63 +87,6 @@ public class Constants {
             m_configuration.MotionMagic.MotionMagicCruiseVelocity = 10;
             m_configuration.MotionMagic.MotionMagicAcceleration = 10;
             m_configuration.MotionMagic.MotionMagicJerk = 10;
-
-            m_configuration.CurrentLimits.SupplyCurrentLimit = 20;
-            m_configuration.CurrentLimits.SupplyCurrentThreshold = 40;
-            m_configuration.CurrentLimits.SupplyTimeThreshold = 0.1;
-            m_configuration.CurrentLimits.SupplyCurrentLimitEnable = true;
-            m_configuration.CurrentLimits.StatorCurrentLimit = 70;
-            m_configuration.CurrentLimits.StatorCurrentLimitEnable = true;
-
-            return m_configuration;
-        }
-    }
-
-    public static class FieldConstants {
-
-        public static final Pose2d BLUE_SPEAKER = new Pose2d(Units.inchesToMeters(-1.5 + 12),Units.inchesToMeters(218.42), new Rotation2d(0));
-        public static final Pose2d RED_SPEAKER = new Pose2d(Units.inchesToMeters(652.73 - 12),Units.inchesToMeters(218.42), new Rotation2d(Math.PI));
-        public static final Pose2d BLUE_FEED = new Pose2d(1.25, 6.62, new Rotation2d(0));
-        public static final Pose2d RED_FEED = new Pose2d(15.250, 6.62, new Rotation2d(0));
-        public static final Pose2d BLUE_AMP = new Pose2d(Units.inchesToMeters(72.5), Units.inchesToMeters(323.00),new Rotation2d(Math.PI / 2));
-        public static final Pose2d RED_AMP = new Pose2d(Units.inchesToMeters(578.77), Units.inchesToMeters(323.00),new Rotation2d(-Math.PI / 2));
-        public static final double BLUE_AUTO_PENALTY_LINE = 9; // X distance from origin to center of the robot almost fully crossing the midline
-        public static final double RED_AUTO_PENALTY_LINE = 7.4; // X distance from origin to center of the robot almost fully crossing the midline
-
-        public static final Rotation2d ampAngle = new Rotation2d(Math.PI / 2);
-    }
-    
-}
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-package frc.robot;
-
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import frc.robot.generated.TunerConstants;
-//Test
-
-/** Add your docs here. */
-public class Constants {
-
-
-    public static final class ExampleSubsystemConstants {
-        public static final int ID_Motor = 0;
-
-        public static TalonFXConfiguration motorConfig() {
-            TalonFXConfiguration m_configuration = new TalonFXConfiguration();
-
-            m_configuration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-            m_configuration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-            m_configuration.Voltage.PeakForwardVoltage = 12.0;
-            m_configuration.Voltage.PeakReverseVoltage = -12.0;
 
             m_configuration.CurrentLimits.SupplyCurrentLimit = 20;
             m_configuration.CurrentLimits.SupplyCurrentThreshold = 40;
