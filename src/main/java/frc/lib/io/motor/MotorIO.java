cfdbf23df7d19baad7390beed779d0caebc9f80e
CJendantix <cjendantix@proton.me>
2025-06-13 17:51:55 -0400

    Add inputs

--- src/main/java/frc/lib/io/motor/MotorIO.java
+++ src/main/java/frc/lib/io/motor/MotorIO.java
@@ -4,14 +4,51 @@
 
 package frc.lib.io.motor;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import org.littletonrobotics.junction.AutoLog;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
 
 /** Add your docs here. */
 public abstract class MotorIO {
 
-    public class Inputs {
+    @AutoLog
+    abstract class MotorIOInputs {
+        /** Whether the motor is connected. */
+        public boolean connected = false;
+        /** Motor position. */
+        public Angle position = Radians.of(0.0);
+        /** Motor velocity. */
+        public AngularVelocity velocity = RadiansPerSecond.of(0.0);
+        /** Voltage applied to the motor. */
+        public Voltage appliedVoltage = Volts.of(0.0);
+        /** Total supply current to the motor. */
+        public Current supplyCurrent = Amps.of(0.0);
+        /** Torque-producing current. */
+        public Current torqueCurrent = Amps.of(0.0);
+        /** Motor temperature in degrees. */
+        public Temperature temperature = Celsius.of(0.0);
+        /** Error in position (e.g., setpoint - actual). */
+        public Angle positionError = null;
+        /** Error in velocity (e.g., setpoint - actual). */
+        public AngularVelocity velocityError = null;
+        /**
+         * Active trajectory position in rotations (from a motion profile or
+         * trajectory).
+         */
+        public Angle activeTrajectoryPosition = null;
+        /** Active trajectory velocity in rotations per second. */
+        public AngularVelocity activeTrajectoryVelocity = null;
     }
 
     public sealed interface Setpoint {
