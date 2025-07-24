1c16a9fc65f9dfbe4956d1b640b387bbf8322ac1
spybh66 <brysonh99@gmail.com>
2025-07-24 19:15:48 -0400

    init

---
+++ src/main/java/frc/lib/util/GamePieceVisualizer.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.lib.util;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import lombok.Getter;
+import lombok.Setter;
+
+/** Add your docs here. */
+public class GamePieceVisualizer {
+
+    @Getter
+    @Setter
+    Pose3d gamePiecePose = new Pose3d();
+}
