0c9fd9e6cfaff329d0a70604df6caaeeafb840d8
jw00541 <wightjjohn@gmail.com>
2025-07-31 21:08:31 -0400

    7/31-syncing to investigate build error

--- src/main/java/frc/robot/subsystems/DetectionML/DetectionML.java
+++ src/main/java/frc/robot/subsystems/DetectionML/DetectionML.java
@@ -13,24 +13,25 @@
  * not, see <https://www.gnu.org/licenses/>.
  */
 
-package frc.robot.subsystems.DetectionML;
+// package frc.robot.subsystems.DetectionML;
 
-import frc.lib.io.DetectionML.DetectionMLIO;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.lib.io.DetectionML.DetectionMLIO;
+// import frc.lib.io.DetectionML.DetectionMLIOAutoLogged;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DetectionML extends SubsystemBase {
-    private final DetectionMLIO inputs;
+// public class DetectionML extends SubsystemBase {
+// private final DetectionMLIOAutoLogged inputs;
 
-    public DetectionML(DetectionMLIO io)
-    {
-        this.inputs = io;
+// public DetectionML(DetectionMLIO io)
+// {
+// this.inputs = io;
 
-        // Initialize inputs
-        this.inputs = new VisionIOInputsAutoLogged[io.length];
-        for (
+// // Initialize inputs
+// this.inputs = new DetectionMLIOAutoLogged[io.length]; // here
+// for (
 
-            int i = 0; i < inputs.length; i++) {
-            inputs[i] = new VisionIOInputsAutoLogged();
-        }
-    }
-}
+// int i = 0; i < inputs.length; i++) {
+// inputs[i] = new VisionIOInputsAutoLogged();
+// }
+// }
+// }
