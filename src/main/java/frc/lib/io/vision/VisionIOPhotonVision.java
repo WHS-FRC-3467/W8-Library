2310b044ca0b87bc99f92b32c784da005818d1b5
CJendantix <cjendantix@proton.me>
2025-08-12 18:12:59 -0400

    better

--- src/main/java/frc/lib/io/vision/VisionIOPhotonVision.java
+++ src/main/java/frc/lib/io/vision/VisionIOPhotonVision.java
@@ -27,6 +27,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import org.apache.commons.lang3.ArrayUtils;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
@@ -96,16 +97,7 @@ public class VisionIOPhotonVision implements VisionIO {
                     averageDistance));
         }
 
-        // inputs.poseObservations = (PoseObservation[]) estimates.toArray();
-        inputs.poseObservations = new PoseObservation[estimates.size()];
-        for (int i = 0; i < estimates.size(); i++) {
-            inputs.poseObservations[i] = estimates.get(i);
-        }
-        // inputs.tagIds = ArrayUtils.toPrimitive((Integer[]) tagIDs.toArray());
-        inputs.tagIds = new int[tagIDs.size()];
-        int i = 0;
-        for (int id : tagIDs) {
-            inputs.tagIds[i++] = id;
-        }
+        inputs.poseObservations = estimates.toArray(new PoseObservation[0]);
+        inputs.tagIds = ArrayUtils.toPrimitive(tagIDs.toArray(new Integer[0]));
     }
 }
