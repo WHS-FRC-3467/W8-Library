16d26db822038cd02e1a6730ba2ec056c044feb5
CJendantix <cjendantix@proton.me>
2025-09-19 08:44:07 -0400

    Increase test duration to account for potential differences in simulations

--- src/test/java/frc/robot/subsystems/rotary/RotaryTest.java
+++ src/test/java/frc/robot/subsystems/rotary/RotaryTest.java
@@ -1,17 +1,16 @@
-/* Copyright (C) 2025 Windham Windup
+/*
+ * Copyright (C) 2025 Windham Windup
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
  */
 
 package frc.robot.subsystems.rotary;
@@ -30,7 +29,8 @@ public class RotaryTest implements AutoCloseable {
     RotarySubsystem rotary;
 
     @BeforeEach // this method will run before each test
-    void setup() {
+    void setup()
+    {
         assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
 
         rotary = new RotarySubsystem(RotarySubsystemConstants.getSim());
@@ -38,20 +38,22 @@ public class RotaryTest implements AutoCloseable {
         /* enable the robot */
         DriverStationSim.setEnabled(true);
         DriverStationSim.notifyNewData();
-      
+
         /* delay ~100ms so the devices can start up and enable */
         Timer.delay(0.100);
     }
-  
+
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     @AfterEach // this method will run after each test
-    void shutdown() throws Exception {
-      close();
+    void shutdown() throws Exception
+    {
+        close();
     }
- 
+
     @Test
-    void goToGoal() {
-        TestUtil.runTest(rotary.setSetpoint(RotarySubsystem.Setpoint.RAISED), 1.5, rotary);
+    void goToGoal()
+    {
+        TestUtil.runTest(rotary.setSetpoint(RotarySubsystem.Setpoint.RAISED), 2, rotary);
         try {
             // Check to see if Rotary subsystem is within tolerance of RAISED setpoint.
             assertTrue(rotary.nearGoal(RotarySubsystem.Setpoint.RAISED.getSetpoint()));
@@ -61,18 +63,20 @@ public class RotaryTest implements AutoCloseable {
     }
 
     @Test // marks this method as a test
-    void goToGoalWithWait() {
-        TestUtil.runTest(rotary.setGoalCommandWithWait(RotarySubsystem.Setpoint.STOW), 1.5, rotary);
+    void goToGoalWithWait()
+    {
+        TestUtil.runTest(rotary.setGoalCommandWithWait(RotarySubsystem.Setpoint.STOW), 2, rotary);
         try {
             // Check position to check if the subsystem is actually in tolerance of STOW setpoint.
             assertTrue(rotary.nearGoal(RotarySubsystem.Setpoint.STOW.getSetpoint()));
-		} catch (Exception e) {
-			fail("Failed to run Rotary Subsystem to STOW: " + e.getMessage());
+        } catch (Exception e) {
+            fail("Failed to run Rotary Subsystem to STOW: " + e.getMessage());
         }
     }
-    
+
     @Override
-    public void close() {
-       rotary.close();
+    public void close()
+    {
+        rotary.close();
     }
 }
