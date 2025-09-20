17f32cea5da3744b7d1efab11228f25ef286636c
CJendantix <cjendantix@proton.me>
2025-09-20 10:42:23 -0400

    Remove setDistance method from DistanceSensorIO

--- src/main/java/frc/lib/io/distancesensor/DistanceSensorIOSim.java
+++ src/main/java/frc/lib/io/distancesensor/DistanceSensorIOSim.java
@@ -16,7 +16,6 @@
 package frc.lib.io.distancesensor;
 
 import static edu.wpi.first.units.Units.Inches;
-import static edu.wpi.first.units.Units.Meters;
 import edu.wpi.first.units.measure.Distance;
 import frc.lib.util.LoggedTunableNumber;
 import lombok.Getter;
@@ -47,7 +46,6 @@ public class DistanceSensorIOSim implements DistanceSensorIO {
      *
      * @param distance The new distance readout
      */
-    @Override
     public void setDistance(Distance distance)
     {
         this.distance = distance;
