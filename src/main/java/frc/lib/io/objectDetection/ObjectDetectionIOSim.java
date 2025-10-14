f17e262a84f09e5b86a7e20eef722de97ac2e4eb
jw00541 <wightjjohn@gmail.com>
2025-10-14 18:21:12 -0400

    Object Detection Sim Layer (#180)
    * Feedback on detection_ML_IO
    
    * Secondary review for detectionML package.
    
    * PhotonVision class via dMLIO
    
    * review
    
    * fixed naming
    
    * test
    
    * reverting test
    
    * 7/31
    
    * 7/31-syncing to investigate build error
    
    * refactored to fix list vs array issue, added placeholder files for device level and subsystem level classes
    
    * fixed merge issues
    
    * Rename detectionMLIO.java to DetectionMLIO.java
    
    * Rename detectionMLIOPhotonVision.java to DetectionMLIOPhotonVision.java
    
    * sync 8/5
    
    * 8/5 sync
    
    * 8/6 ML detection device-level groundwork & clarifying comments within IO layers
    
    * Methods check
    
    * method check
    
    * range/heading method checks 8/11
    
    * range/heading method checks 8/11
    
    * range/heading checks 8/11
    
    * 8/11
    
    * updated math for camera range function to be valid for installation pitch up or down; see comments.
    
    * Changed units to meters, added Translation3d & distance functions
    
    * camera sim changes
    
    * added ability to create more than 1 target and moved static numbers to constants file
    
    * test heading in sim
    
    * use this to check heading & distance in sim
    
    * in progress real calcs
    
    * 09/04 range/heading sim test updates
    
    * Rename detectionMLIO.java to DetectionMLIO.java
    
    * Rename detectionMLIOPhotonVision.java to DetectionMLIOPhotonVision.java
    
    * added wireframe render for sim
    
    * Bryson check heading, this was working before but isn't now?
    
    * 09/18/2025 meet changes. range/heading made relative to robot vs camera, range/heading calcs corrected to properly allow for non-zero camera transform
    
    * yawCorrection added for range calculation when camera has non-zero yaw. signs for camera transform made consistent with underlying mathematics in formula. still need to test & make similar changes to heading calc.
    
    * bryson test
    
    * fixed target translation to work with robot rotation
    
    * finalizing installation yaw logic (9/23 meet)
    
    * @bryson test with different camera transforms (different translations, different pitches/yaws) for another set of eyes on how it behaves. seems reasonable, even with yaw, until limits of FOV. no yaw case seems near perfect and no pitch/yaw case seems virtually perfect. haven't tested with robot swerve approach yet.
    
    * added targetToFieldTranslation function -- comments and optimizations still in flux
    
    * testing detected objects list method
    
    * testing getLastNDetections
    
    * 10/6 to-do
    
    * vision -> machineVision to differentiate from fiducial to resolve conflict.
    
    * 10/9
    
    * Updating lastNDetections list logic
    
    * Changing naming convention to be consistent with IO layer & adding doc-strings
    
    * Removal of old files
    
    * Adding for sub-PR then removing before merging this one
    
    ---------
    
    Co-authored-by: CJendantix <cjendantix@proton.me>
    Co-authored-by: spybh66 <brysonh99@gmail.com>
    Co-authored-by: Bryson Halsey <Spybh66@users.noreply.github.com>

---
+++ src/main/java/frc/lib/io/objectDetection/ObjectDetectionIOSim.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.lib.io.objectDetection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.Logger;
+import org.photonvision.PhotonCamera;
+import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.simulation.SimCameraProperties;
+import org.photonvision.simulation.VisionSystemSim;
+import org.photonvision.simulation.VisionTargetSim;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
+
+/** An object detection sim class that utilizes the PhotonVision implementation for tests. */
+public class ObjectDetectionIOSim extends ObjectDetectionIOPhotonVision {
+    protected final String cameraName;
+    private final VisionSystemSim visionSim;
+    private final PhotonCamera cam;
+    private final PhotonCameraSim camSim;
+    private final Supplier<Pose2d> robotPoseSupplier;
+
+    public ObjectDetectionIOSim(String cameraName, Transform3d cameraTransform,
+        Supplier<Pose2d> robotPoseSupplier,
+        String target_name, VisionTargetSim[] targets)
+    {
+        super(cameraName);
+        this.cameraName = cameraName;
+        // Initialize vision sim
+        cam = new PhotonCamera(cameraName);
+        camSim = new PhotonCameraSim(cam, new SimCameraProperties());
+        // Wireframe visualizer
+        camSim.enableDrawWireframe(true);
+
+        visionSim = new VisionSystemSim("objectML");
+        visionSim.addCamera(camSim, cameraTransform);
+        this.robotPoseSupplier = robotPoseSupplier;
+
+        // TODO: find cleaner impl
+        visionSim.addVisionTargets(target_name, targets);
+        Set<VisionTargetSim> test = visionSim.getVisionTargets();
+        List<VisionTargetSim> targetList = new ArrayList<>(test);
+
+        for (VisionTargetSim target : targetList) {
+            Logger.recordOutput("ALGAE POSE" + targetList.indexOf(target), target.getPose());
+        }
+
+    }
+
+    @Override
+    public void updateInputs(ObjectDetectionIOInputs inputs)
+    {
+        visionSim.update(robotPoseSupplier.get());
+        super.updateInputs(inputs);
+    }
+
+    @Override
+    public String getCamera()
+    {
+        return cameraName;
+    }
+
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -60,6 +60,8 @@ import frc.robot.subsystems.flywheel.Flywheel;
 import frc.robot.subsystems.flywheel.FlywheelConstants;
 import frc.robot.subsystems.leds.LEDs;
 import frc.robot.subsystems.leds.LEDsConstants;
+import frc.robot.subsystems.objectDetector.objectDetector;
+import frc.robot.subsystems.objectDetector.objectDetectorConstants;
 import frc.robot.subsystems.linear.Linear;
 import frc.robot.subsystems.linear.LinearConstants;
 import frc.robot.subsystems.rotary.RotarySubsystem;
@@ -99,6 +101,7 @@ public class RobotContainer {
     private final BeamBreak1 beamBreak1;
     private final Servo1 servo1;
     private final Flywheel flywheel;
+    private final objectDetector objectVision;
     private final Linear linear;
     private final Vision vision;
     private final RotarySubsystem rotary;
@@ -133,6 +136,7 @@ public class RobotContainer {
                 beamBreak1 = new BeamBreak1(BeamBreak1Constants.getReal());
                 servo1 = new Servo1(Servo1Constants.getReal());
                 flywheel = new Flywheel(FlywheelConstants.getReal());
+                objectVision = new objectDetector(objectDetectorConstants.getReal(), drive);
 
                 linear = new Linear(LinearConstants.getReal());
 
@@ -164,6 +168,9 @@ public class RobotContainer {
                     BeamBreak1Constants.getSim());
                 servo1 = new Servo1(Servo1Constants.getSim());
                 flywheel = new Flywheel(FlywheelConstants.getSim());
+                objectVision =
+                    new objectDetector(objectDetectorConstants.getSim(() -> drive.getPose()),
+                        drive);
 
                 linear = new Linear(LinearConstants.getSim());
 
@@ -197,6 +204,7 @@ public class RobotContainer {
                     new BeamBreak1(BeamBreak1Constants.getReplay());
                 servo1 = new Servo1(Servo1Constants.getReplay());
                 flywheel = new Flywheel(FlywheelConstants.getReplay());
+                objectVision = new objectDetector(objectDetectorConstants.getReplay(), drive);
 
                 linear = new Linear(LinearConstants.getReplay());
                 rotary = new RotarySubsystem(RotarySubsystemConstants.getReplay());
