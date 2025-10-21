d126b9d7bbce7ad9b1551615b37ce6b8c44a4013
CJendantix <cjendantix@proton.me>
2025-10-21 18:19:06 -0400

    remove hungarian notation

--- src/main/java/frc/lib/util/LoggedTuneableProfiledPID.java
+++ src/main/java/frc/lib/util/LoggedTuneableProfiledPID.java
@@ -10,7 +10,11 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 public class LoggedTuneableProfiledPID extends ProfiledPIDController {
 
     // Tunable numbers
-    private LoggedTunableNumber m_kP, m_kI, m_kD, m_maxV, m_maxA;
+    private LoggedTunableNumber p;
+    private LoggedTunableNumber i;
+    private LoggedTunableNumber d;
+    private LoggedTunableNumber maxVelocity;
+    private LoggedTunableNumber maxAcceleration;
 
     public LoggedTuneableProfiledPID(String name, double kP, double kI,
         double kD, double maxV, double maxA)
@@ -18,32 +22,33 @@ public class LoggedTuneableProfiledPID extends ProfiledPIDController {
         this(name, kP, kI, kD, maxV, maxA, .02);
     }
 
-    public LoggedTuneableProfiledPID(String name, double kP, double kI,
-        double kD, double maxV, double maxA, double period)
+    public LoggedTuneableProfiledPID(String name, double p, double i,
+        double d, double maxVelocity, double maxAcceleration, double period)
     {
-        super(kP, kI, kD, new TrapezoidProfile.Constraints(maxV, maxA), period);
+        super(p, i, d, new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration), period);
 
         // Tunable numbers for PID and motion gain constants
-        m_kP = new LoggedTunableNumber(name + "/kP", kP);
-        m_kI = new LoggedTunableNumber(name + "/kI", kI);
-        m_kD = new LoggedTunableNumber(name + "/kD", kD);
+        this.p = new LoggedTunableNumber(name + "/kP", p);
+        this.i = new LoggedTunableNumber(name + "/kI", i);
+        this.d = new LoggedTunableNumber(name + "/kD", d);
 
-        m_maxV = new LoggedTunableNumber(name + "/maxV", maxV);
-        m_maxA = new LoggedTunableNumber(name + "/maxA", maxA);
+        this.maxVelocity = new LoggedTunableNumber(name + "/maxVelocity", maxVelocity);
+        this.maxAcceleration = new LoggedTunableNumber(name + "/maxAcceleration", maxAcceleration);
     }
 
     public void updatePID()
     {
         // If changed, update controller constants from Tuneable Numbers
-        if (m_kP.hasChanged(hashCode())
-            || m_kI.hasChanged(hashCode())
-            || m_kD.hasChanged(hashCode())) {
-            this.setPID(m_kP.get(), m_kI.get(), m_kD.get());
+        if (p.hasChanged(hashCode())
+            || i.hasChanged(hashCode())
+            || d.hasChanged(hashCode())) {
+            this.setPID(p.get(), i.get(), d.get());
         }
 
-        if (m_maxV.hasChanged(hashCode())
-            || m_maxA.hasChanged(hashCode())) {
-            this.setConstraints(new TrapezoidProfile.Constraints(m_maxV.get(), m_maxA.get()));
+        if (maxVelocity.hasChanged(hashCode())
+            || maxAcceleration.hasChanged(hashCode())) {
+            this.setConstraints(
+                new TrapezoidProfile.Constraints(maxVelocity.get(), maxAcceleration.get()));
         }
     }
 
