3c7fa0fbbddc1ef8b1c64a4376dce539e2572b94
CJendantix <cjendantix@proton.me>
2025-11-10 12:23:06 -0500

    Remove testing trig pose

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -47,8 +47,6 @@ public class RobotState {
     @Getter
     @AutoLogOutput(key = "Odometry/Robot")
     private Pose2d estimatedPose = Pose2d.kZero;
-    @AutoLogOutput(key = "Odometry/Test")
-    private Pose2d testPose = Pose2d.kZero;
     @Getter
     private Optional<TagObservation> closestTagObservation = Optional.empty();
 
@@ -59,7 +57,6 @@ public class RobotState {
     public void addOdometryObservation(OdometryObservation observation) {
         poseEstimator.addOdometryObservation(observation);
         estimatedPose = poseEstimator.estimatedPose();
-        testPose = poseEstimator.getTrigPose(10).orElse(Pose2d.kZero);
     }
 
     public void addVisionObservation(VisionObservation observation) {
