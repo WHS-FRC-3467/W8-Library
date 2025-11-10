21ff9c0ac1e79387c2317304e5a45fd33a45fd11
CJendantix <cjendantix@proton.me>
2025-11-10 16:34:03 -0500

    Revert "Fix package naming for objectDetection imports"
    This reverts commit ff3d264250c87ba12421a92698a661be2a1fa0f3.

--- src/main/java/frc/lib/io/objectDetection/ObjectDetectionIO.java
+++ src/main/java/frc/lib/io/objectDetection/ObjectDetectionIO.java
@@ -13,7 +13,7 @@
  * not, see <https://www.gnu.org/licenses/>.
  */
 
-package frc.lib.io.objectDetection;
+package frc.lib.io.objectdetection;
 
 import java.util.List;
 import org.littletonrobotics.junction.AutoLog;
