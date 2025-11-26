c0dd1312eebcf8dc3ace029c952202d554f9e208
Garrett Hart <garrett7621@gmail.com>
2025-11-26 16:16:15 -0500

    fix import

--- src/main/java/frc/lib/devices/ObjectDetection.java
+++ src/main/java/frc/lib/devices/ObjectDetection.java
@@ -13,7 +13,7 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import frc.lib.io.objectDetection.ObjectDetectionIOInputsAutoLogged;
+import frc.lib.io.objectdetection.ObjectDetectionIOInputsAutoLogged;
 import frc.lib.io.objectdetection.ObjectDetectionIO;
 import frc.lib.io.objectdetection.ObjectDetectionIO.TargetObservation;
 
