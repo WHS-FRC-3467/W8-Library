80bbc1f91bba67adf375eecce323e221b8a90bbe
CJendantix <cjendantix@proton.me>
2025-12-05 18:05:25 -0500

    Fix unimported method

--- src/test/java/frc/robot/subsystems/flywheel/FlywheelTest.java
+++ src/test/java/frc/robot/subsystems/flywheel/FlywheelTest.java
@@ -15,7 +15,7 @@
 package frc.robot.subsystems.flywheel;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import edu.wpi.first.hal.HAL;
 
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
